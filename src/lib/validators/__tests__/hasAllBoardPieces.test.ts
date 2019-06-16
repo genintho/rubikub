edb6459faf7480d7f58af1bfe92abafc046fe768
Thomas Genin <xt6.thomas.genin@gmail.com>
2019-06-16 00:14:25 -0700

    Test type

--- src/lib/validators/__tests__/hasAllBoardPieces.test.ts
+++ src/lib/validators/__tests__/hasAllBoardPieces.test.ts
@@ -7,20 +7,20 @@ describe("hasAllBoardPieces", () => {
         expect(res).toBeTruthy();
     });
     it("should return true when all are seen", () => {
-        const res = hasAllBoardPieces([[new TileModel(1, "red", 1)]], []);
+        const res = hasAllBoardPieces([[new TileModel(1, "red", "1")]], []);
         expect(res).toBeTruthy();
     });
     it("should be happy identical", () => {
         const res = hasAllBoardPieces(
-            [[null, new TileModel(1, "red", 1)]],
-            [[new TileModel(1, "red", 1), null]]
+            [[null, new TileModel(1, "red", "1")]],
+            [[new TileModel(1, "red", "1"), null]]
         );
         expect(res).toBeTruthy();
     });
     it("should return false when some are missing", () => {
         const res = hasAllBoardPieces(
-            [[new TileModel(1, "red", 1)]],
-            [[new TileModel(0, "red", 1)]]
+            [[new TileModel(1, "red", "1")]],
+            [[new TileModel(0, "red", "1")]]
         );
         expect(res).toBeFalsy();
     });
